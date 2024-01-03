c5947c6efb9f50c25be9f04636257e4307e3b4bc
Vishnuvp26 <vishnumohanmfc@gmail.com>
2024-01-03 16:59:07 +0530

    updates

--- Patterns vscode 1/HollowSqare.c
+++ Patterns vscode 1/HollowSqare.c
@@ -6,7 +6,7 @@ int main()
     for(i=1;i<=5;i++){
         for(j=1;j<=5;j++){
 
-            if(i==1||i==5||j==1||j==5)
+            if(i==1||i==5||j==1||j==5) 
             printf("*");
 
             else
--- Patterns vscode 1/StarDiamond.c
+++ Patterns vscode 1/StarDiamond.c
@@ -5,7 +5,7 @@ int main()
     int i,j,k;
     
     for(i=1;i<=7;i++){
-       i<=4?k++:k--;
+       i<=4?k++:k--; //if else
         for(j=1;j<=7;j++){
             if(j>=5-k&&j<=3+k){
             printf("*");
